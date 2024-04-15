2a8cde76cb056a668334bb068b6d2c5e1cd44e51
alevin93 <79819155+alevin93@users.noreply.github.com>
2024-04-15 11:41:55 -0700

    Update Input.js

--- src/components/Input.js
+++ src/components/Input.js
@@ -15,7 +15,7 @@ function Input() {
         "data" : `${input}`,
         "comment" : `${comment}`
       };
-      const response = await fetch(`10.182.0.2:5500/submit`, {
+      const response = await fetch(`http://localhost:5500/submit`, {
         method: 'POST',
         headers: {
           'Content-Type': "application/json"
