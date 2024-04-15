733ec11ba5536bf53273143d999d6a1667b4d1f4
alevin93 <79819155+alevin93@users.noreply.github.com>
2024-04-15 12:17:54 -0700

    Update Body.js

--- src/components/Body.js
+++ src/components/Body.js
@@ -18,7 +18,7 @@ function Body() {
 
   async function fetchGames() {
     try {
-      const response = await fetch(`http://10.182.0.2:5500/getFriendsData`, {
+      const response = await fetch(`http://127.0.0.1:5500/getFriendsData`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ user: localStorage.getItem('user') }) 
